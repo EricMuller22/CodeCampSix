d307a5b18d1f3e8963a766581f5e08794319c059
Eric Muller <emuller@squareup.com>
2016-07-12 11:13:18 -0700

    Addressing feedback

--- Stock.playground/Contents.swift
+++ Stock.playground/Contents.swift
@@ -1,149 +1,42 @@
 import Foundation
 
 
-struct StockGrant
-{
-    let shares: UInt
-    let strikePrice: Double
-    let grantDate: Date
-    let cliffMonths: UInt
-    let endMonths: UInt
-
-    // RSU grants will use default value for strikePrice, NSO grants will set a strike price.
-    init(shares: UInt, grantDate: Date, cliffMonths: UInt, endMonths: UInt, strikePrice: Double = 0)
-    {
-        self.strikePrice = strikePrice
-        self.shares = shares
-        self.grantDate = grantDate
-        self.cliffMonths = cliffMonths
-        self.endMonths = endMonths
-    }
-
-    // Bonus grants: vest instantly
-    init(shares: UInt, grantDate: Date, strikePrice: Double = 0)
-    {
-        self.shares = shares
-        self.strikePrice = strikePrice
-        self.grantDate = grantDate
-        cliffMonths = 0
-        endMonths = 0
-    }
-
-    func valueAt(_ price: Double, monthsElapsed months: UInt) -> Double
-    {
-        guard months >= cliffMonths else { return 0 }
-        guard months <= endMonths else { return valueAt(price, sharesAccrued: Double(shares)) }
-        guard price > strikePrice else { return 0 }
-
-        let percentageAccrued = Double(months) / Double(endMonths)
-        let sharesAccrued = floor(percentageAccrued * Double(shares))
-        return valueAt(price, sharesAccrued: sharesAccrued)
-    }
-
-    func valueAt(_ price: Double, date: Date) -> Double
-    {
-        let monthsElapsed = date.monthsSince(grantDate)
-        if (monthsElapsed < 0) { return 0 }
-        return valueAt(price, monthsElapsed: UInt(monthsElapsed))
-    }
-
-    private func valueAt(_ price: Double, sharesAccrued: Double) -> Double
-    {
-        let shareValue = price - strikePrice
-        let value = max(0, shareValue)
-        let shareCount = max(0, min(Double(shares), sharesAccrued))
-        return value * shareCount
-    }
-}
-
-
-extension Date
-{
-    static func grantDate(month: Int, day: Int, year: Int) -> Date
-    {
-        let signing = NSDateComponents.init()
-        signing.calendar = Calendar.grantDateCalendar()
-        signing.month = month
-        signing.day = day
-        signing.year = year
-        return signing.date!
-    }
-
-    func monthsSince(_ date: Date) -> Int
-    {
-        let dateComponents = Calendar.grantDateCalendar().components(.month, from: date, to: self)
-        return dateComponents.month!
-    }
-
-    func after(months: Int) -> Date
-    {
-        return after(months, unit: .month)
-    }
-
-    func after(years: Int) -> Date
-    {
-        return after(years, unit: .year)
-    }
-
-    private func after(_ amount: Int, unit: Calendar.Unit) -> Date
-    {
-        return Calendar.grantDateCalendar().date(byAdding: unit, value: amount, to: self)!
-    }
-}
-
-
-extension Calendar
-{
-    static func grantDateCalendar() -> Calendar
-    {
-        return Calendar(calendarIdentifier: .gregorian)!
-    }
-}
-
-
 let signing = Date.grantDate(month: 1, day: 1, year: 2016)
-let signingGrant = StockGrant(shares: 20000, grantDate: signing, cliffMonths: 12, endMonths: 48, strikePrice: 1.22)
+let signingGrant = StockGrant(shares: 10000, grantDate: signing, strikePrice: 1.00)
 
-let twoYears = signing.after(years: 2)
-let fourYears = signing.after(years: 4)
+let twoYears = signing.after(.year(2))
+let fourYears = signing.after(.year(4))
 
 // Before cliff: should be zero
-signingGrant.valueAt(22, monthsElapsed: signingGrant.cliffMonths - 1)
+signingGrant.value(at: 10, after: .month(11))
 
 // Zero months elapsed: should be zero
-assert(signingGrant.cliffMonths > 0)
-signingGrant.valueAt(22, monthsElapsed: 0)
+signingGrant.value(at: 10, after: .month(0))
 
-// Negative years (invalid): should be zero
-let negativeYears = signing.after(years: -1)
-signingGrant.valueAt(22, date: negativeYears)
+// Two years: should be 50k
+signingGrant.value(at: 11, after: .month(24))
+signingGrant.value(at: 11, after: .year(2))
+signingGrant.value(at: 11, onDate: twoYears)
 
-// Progression up to end date:
-signingGrant.valueAt(22, monthsElapsed: 24)
-signingGrant.valueAt(22, date: twoYears)
-signingGrant.valueAt(22, monthsElapsed: 48)
-signingGrant.valueAt(22, date: fourYears)
+// Four years: should be 100k
+signingGrant.value(at: 11, after: .month(48))
+signingGrant.value(at: 11, after: .year(4))
+signingGrant.value(at: 11, onDate: fourYears)
 
 // After end date: should be equal to end date value
-signingGrant.valueAt(22, monthsElapsed: signingGrant.endMonths)
-signingGrant.valueAt(22, monthsElapsed: signingGrant.endMonths + 1)
-signingGrant.valueAt(22, date: signingGrant.grantDate.after(months: Int(signingGrant.endMonths)))
-signingGrant.valueAt(22, date: signingGrant.grantDate.after(months: Int(signingGrant.endMonths + 1)))
+signingGrant.value(at: 11, after: .month(49))
+signingGrant.value(at: 11, onDate: signing.after(.month(49)))
+signingGrant.value(at: 11, after: .year(5))
 
-let rsuGrant = StockGrant(shares: 10000, grantDate: twoYears, cliffMonths: 12, endMonths: 48)
-rsuGrant.valueAt(22, monthsElapsed:rsuGrant.cliffMonths - 1)
-rsuGrant.valueAt(22, monthsElapsed:rsuGrant.cliffMonths)
-rsuGrant.valueAt(11, monthsElapsed:rsuGrant.endMonths)
-rsuGrant.valueAt(22, monthsElapsed:rsuGrant.endMonths)
-rsuGrant.valueAt(22, monthsElapsed:rsuGrant.endMonths + 1)
+let rsuGrant = StockGrant(shares: 10000, grantDate: twoYears)
+rsuGrant.value(at: 10, after:.month(11))
+rsuGrant.value(at: 10, after:.month(12))
+rsuGrant.value(at: 10, after:.year(1))
+rsuGrant.value(at: 10, after:.month(48))
+rsuGrant.value(at: 10, after:.month(48))
+rsuGrant.value(at: 10, after:.month(49))
 
-let bonusGrant = StockGrant(shares: 2000, grantDate: fourYears)
-bonusGrant.valueAt(22, date: bonusGrant.grantDate.after(months: -1))
-bonusGrant.valueAt(11, monthsElapsed: 0)
-bonusGrant.valueAt(22, monthsElapsed: 0)
-bonusGrant.valueAt(22, monthsElapsed: 20)
-
-// Poke at private implementation details a bit:
-signingGrant.valueAt(-1, sharesAccrued: 100)
-signingGrant.valueAt(22, sharesAccrued: -1)
-signingGrant.valueAt(22, sharesAccrued: Double(signingGrant.shares + 1))
+let bonusGrant = StockGrant(shares: 10000, grantDate: fourYears, vestingSchedule: .Instant)
+bonusGrant.value(at: 10, after: .month(0))
+bonusGrant.value(at: 20, after: .month(0))
+bonusGrant.value(at: 20, after: .month(20))
--- Words.playground/Contents.swift
+++ Words.playground/Contents.swift
@@ -6,90 +6,22 @@
 import Foundation
 
 
-struct WordCounter
-{
-    let wordCounts: NSCountedSet
-    let sortedWordCounts: [String]
-
-    init(text: String)
-    {
-        wordCounts = WordCounter.wordCountsForString(text)
-        sortedWordCounts = WordCounter.sortedWordCountsWithCounts(wordCounts)
-    }
-
-    func wordCountForWord(word: String) -> Int
-    {
-        let cleanWord = WordCounter.sanitizedWord(word)
-        if cleanWord.isEmpty { return 0 }
-        return wordCounts.countForObject(cleanWord)
-    }
-
-    func nthMostFrequentWord(n: Int) -> String
-    {
-        guard 0 < n else { return "" }
-        guard n <= sortedWordCounts.count else { return "" }
-        return sortedWordCounts[n - 1];
-    }
-
-    func topFilteredWords(n: Int, filter:(String)->Bool) -> Array<String> {
-        let filteredWords = sortedWordCounts.filter({ filter($0) })
-        return Array(filteredWords[0...n-1])
-    }
-}
-
-
-extension WordCounter
-{
-    static func sanitizedStringComponents(text: String) -> [String]
-    {
-        return text .componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-            .map { $0.lowercaseString.stringByTrimmingCharactersInSet(NSCharacterSet.punctuationCharacterSet()) }
-            .filter { !$0.isEmpty }
-    }
-
-    static func sanitizedWord(word: String) -> String
-    {
-        let words = sanitizedStringComponents(word)
-        return words.count == 1 ? words.first! : ""
-    }
-
-    static func wordCountsForString(text: String) -> NSCountedSet
-    {
-        return NSCountedSet.init(array: sanitizedStringComponents(text))
-    }
-
-    static func sortedWordCountsWithCounts(counts: NSCountedSet) -> [String]
-    {
-        guard let wordCounts = counts.allObjects as? [String] else { return [] }
-        return wordCounts.sort({ (firstWord: String, secondWord: String) -> Bool in
-            let firstCount = counts.countForObject(firstWord)
-            let secondCount = counts.countForObject(secondWord)
-            if firstCount == secondCount {
-                return firstWord < secondWord
-            }
-            return firstCount > secondCount
-        })
-    }
-}
-
-
-
 let runGatsby = true
 
 if (runGatsby) {
-    let gatsbyFile = NSBundle.mainBundle().URLForResource("gatsby", withExtension: "txt")!
-    let gatsbyText = try String(contentsOfURL: gatsbyFile)
+    let gatsbyFile = Bundle.main.urlForResource("gatsby", withExtension: "txt")!
+    let gatsbyText = try String(contentsOf: gatsbyFile)
     let gatsbyCounter = WordCounter(text: gatsbyText)
-    gatsbyCounter.sortedWordCounts
-    gatsbyCounter.wordCountForWord("Daisy")
+    gatsbyCounter.uniqueWords
+    gatsbyCounter.wordCount(for: "Daisy")
     gatsbyCounter.nthMostFrequentWord(1)
 
     let topWords = gatsbyCounter.topFilteredWords(100, filter: {(word: String)->Bool in return true })
     topWords
 
-    let colors = NSSet(array: ["red", "orange", "blue", "green", "yellow", "indigo", "violet", "black", "white", "gold"])
+    let colors: Set = ["red", "orange", "blue", "green", "yellow", "indigo", "violet", "black", "white", "gold"]
     let topColors = gatsbyCounter.topFilteredWords(7, filter: {(word: String)->Bool in
-        return colors.containsObject(word)
+        return colors.contains(word)
     })
 } else {
     let testString = "Then wear the gold hat, if that will move her;\n" +
@@ -100,18 +32,18 @@ if (runGatsby) {
 
     let wordCounter = WordCounter(text: testString)
 
-    wordCounter.wordCountForWord("Yo")
-    wordCounter.wordCountForWord("")
-    wordCounter.wordCountForWord("That isn't a word")
-    wordCounter.wordCountForWord("lOvEr,")
-    wordCounter.wordCountForWord("Gold-Hatted?")
+    wordCounter.wordCount(for: "Yo")
+    wordCounter.wordCount(for: "")
+    wordCounter.wordCount(for: "That isn't a word")
+    wordCounter.wordCount(for: "lOvEr,")
+    wordCounter.wordCount(for: "Gold-Hatted?")
 
     wordCounter.nthMostFrequentWord(0)
     wordCounter.nthMostFrequentWord(1)
     wordCounter.nthMostFrequentWord(2)
     wordCounter.nthMostFrequentWord(3)
     wordCounter.nthMostFrequentWord(4)
-    let largestN = wordCounter.sortedWordCounts.count
+    let largestN = wordCounter.uniqueWords
     wordCounter.nthMostFrequentWord(largestN)
     wordCounter.nthMostFrequentWord(largestN + 1)
 }
